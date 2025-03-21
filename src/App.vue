a86bb1c405cb068d98aa68c34f8c579cad545fe0
edgarmauricio123 <22620066@tlaxiaco.tecnm.mx>
2025-03-21 13:45:01 -0600

    Agregiue Descripcion y carrusel .vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup>
 import Navbar from "./components/Navbar/Navbar.vue"
+import Descripcion from "./components/Descripcion/Descripcion.vue"
+import carrusel from "./components/Carrusel/Carrusel.vue"
 </script>
 
 <template>
   
   <Navbar />
+  <Descripcion />
+  <carrusel />
+
 </template>
 
 <style scoped>
---
+++ src/components/Descripcion/Descripcion.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="inicio" class="container-fluid banner">
+        <div class="text-container">
+            <h1>El café que mereces, directo a tu taza</h1>
+            <p>
+                Sumérgete en el mundo del café con granos seleccionados de las mejores fincas, tostados a la
+                perfección para resaltar cada nota de sabor. Desde un espresso intenso hasta un latte cremoso,
+                cada sorbo es una experiencia única. Ya sea que busques energía para
+                empezar el día o un momento de calma, aquí encontrarás el café ideal para ti. ¡Disfruta el auténtico
+                aroma y sabor en cada taza!
+            </p>
+            <button class="btn btn-warning me-2">Saber más</button>
+            <button class="btn btn-outline-light">Contacto</button>
+        </div>
+        <div class="col-md-6 text-center">
+            <img src="" class="img-cafe" alt="Taza de café">
+        </div>
+    </div>
+</template>
